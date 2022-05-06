d15f8c400852e74c1b223e0e803a1794f7851b36
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-05-06 20:20:45 -0300

    feat: finished back-end

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -2,19 +2,59 @@
 //     expect(2 + 2).toBe(4);
 // });
 
+/* SPIES (ESPIÕES): 
+- Maneiras de sabermos dentro dos testes do jest se uma função foi chamada ou não
+*/
+
+const createFeedbackSpy = jest.fn();//função spie
+const sendMailSpy = jest.fn();
+
 import { SubmitFeedbackUseCase } from "./SubmitFeedbackUseCase";
+
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendEmail: sendMailSpy }
+)
 
 describe('Submit feedback', () => {
     it('should be able to submit a feedback', async () => {
-        const submitFeedback = new SubmitFeedbackUseCase(
-            {create: async () => {}},
-            {sendEmail: async () => {}}
-        )
+
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,sjbjasbfjsbfjsj'
+        })).resolves.not.toThrow();// esperar que a função não retorne um erro
+
+
+        // testando se funções espiãs foram chamadas
+        expect(createFeedbackSpy).toHaveBeenCalled();
+        expect(sendMailSpy).toHaveBeenCalled();
+    });
+
+    it('should not be able to submit a feedback without a type', async () => {
+
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,sjbjasbfjsbfjsj'
+        })).rejects.toThrow();// esperar que a função não rode e que de retorne um erro
+    });
+
+    it('should not be able to submit a feedback without a comment', async () => {
+
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64,sjbjasbfjsbfjsj'
+        })).rejects.toThrow();// esperar que a função não rode e que de retorne um erro
+    });
+
+    it('should not be able to submit a feedback with an invalid screenshot', async () => {
 
         await expect(submitFeedback.execute({
             type: 'BUG',
             comment: 'example comment',
             screenshot: 'test.jpg'
-        })).resolves.not.toThrow();// esperar que a função não retorne um erro
+        })).rejects.toThrow();// esperar que a função não rode e que de retorne um erro
     });
 });
